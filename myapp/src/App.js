83129c1b2cdcfb73f0c766bfd596afa99b9418ce
Giga <23157357+Giagar@users.noreply.github.com>
2020-08-13 18:53:30 +0200

    section participants done

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -11,17 +11,23 @@ class App extends Component {
       name: "Myname",
       year: 2020,
     },
-    participants: {
-      
-    },
+    participants: [
+      { name: "Person1", country: "Italy", id:1 },
+      { name: "Person2", country: "France", id:2 },
+      { name: "Person3", country: "The UK", id:3 },
+      { name: "Person4", country: "Spain", id:4 },
+    ],
   };
 
   render() {
     return (
       <div className="App">
         <Header />
-        <Participants />
-        <Footer name={this.state.copyright.name} year={this.state.copyright.year} />
+        <Participants participants={this.state.participants} />
+        <Footer
+          name={this.state.copyright.name}
+          year={this.state.copyright.year}
+        />
       </div>
     );
   }
--- myapp/src/Participants.js
+++ myapp/src/Participants.js
@@ -1,14 +1,19 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Participants extends Component {
-    render() {
-        return(
-            <section>
-                <h2>Participants</h2>
-                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque enim illum eius, blanditiis sunt hic nisi atque obcaecati minus culpa? Rem accusamus aspernatur consequuntur corporis debitis provident nobis optio magni.</div>
-            </section>
-        )
-    }
+  render() {
+
+    const list = this.props.participants.map((person, index) => <li key={person.id}>The participant n. {index + 1} is {person.name} from {person.country}</li>)
+
+    return (
+      <section>
+        <h2>Participants</h2>
+        <div>
+            <ul>{list}</ul>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Participants;
