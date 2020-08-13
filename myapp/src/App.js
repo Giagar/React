c46e5fa5e2233dce3c16b8d48cacba45501fea68
Giga <23157357+Giagar@users.noreply.github.com>
2020-08-13 13:36:22 +0200

    add basic structure: header, section participants, footer

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Participants from "./Participants";
 
 class App extends Component {
+  state = {
+    copyright: {
+      name: "Myname",
+      year: 2020,
+    },
+    participants: {
+      
+    },
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Ready to start</h1>
+        <Header />
+        <Participants />
+        <Footer name={this.state.copyright.name} year={this.state.copyright.year} />
       </div>
     );
   }
---
+++ myapp/src/Participants.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Participants extends Component {
+    render() {
+        return(
+            <section>
+                <h2>Participants</h2>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque enim illum eius, blanditiis sunt hic nisi atque obcaecati minus culpa? Rem accusamus aspernatur consequuntur corporis debitis provident nobis optio magni.</div>
+            </section>
+        )
+    }
+}
+
+export default Participants;
